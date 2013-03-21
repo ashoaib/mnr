aaf83e1b5863b73f6076e39cc241ed8ffdb42a4e
ashoaib <abu.rm.shoaib@gmail.com>
2013-03-21 22:13:13 +0000

    Added js compiling

--- compile.sh
+++ compile.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #set -e
 css_dir=css
+js_dir=~/Documents/Programming/Projects/marriyah-raihan/js
 echo "Compiling css in $css_dir ... "
 sass --style compressed $css_dir/main.scss $css_dir/main.css
+echo "Done."
+echo "Compiling js in $js_dir"
+python $PYTHONLIB/google_closure_compiler.py $js_dir scripts-min.js
 echo "Done."
 #set +e
