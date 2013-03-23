0b15c33714f2b06ce2baa058569315a3877b44d7
ashoaib <abu.rm.shoaib@gmail.com>
2013-03-23 17:01:18 +0000

    Updated HTML to use minified js

--- compile.sh
+++ compile.sh
@@ -6,6 +6,6 @@ echo "Compiling css in $css_dir ... "
 sass --style compressed $css_dir/main.scss $css_dir/main.css
 echo "Done."
 echo "Compiling js in $js_dir"
-python $PYTHONLIB/google_closure_compiler.py $js_dir scripts-min.js
+python $PYTHONLIB/google_closure_compiler.py $js_dir min/scripts-min.js
 echo "Done."
 #set +e
